351d4ca92ba0eb84927f3be7c375ff3881101c00
AnhhPhamhocIT <11236058@st.neu.edu.vn>
2025-03-17 14:38:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-<<<<<<< HEAD
-print('hello')
-i = 12
-a = 13
-print(i+a)
-how to use github
-????
-=======
-import flask from Flask
->>>>>>> a322757 (First commit)
+import os
+import sys
+ def main():
+    """Hàm chính để chạy ứng dụng Django."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+    #Đặt biến môi trường chỉ định file cấu hình của Django
+    
+    try:
+    	form django.core.management import execute_from_command_line
+        #Import hàm chạy lệnh django
+        except ImportError as exc:
+            raise ImportError(
+            	"Không thể import Django. Hãy kiểm tra xem Django đã được cài đặt chưa."
+                ) form exc
+                
+      execute_from_command_line(sys.argv)
+    	# Chạy lệnh từ terminal (ví dụ: 'python main.py runserver')
+        
+        if __name_-== "__main__":
+            main()
+            
+            
